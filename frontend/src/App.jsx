4e2bd1a65b7e3079f2a43540debb0951145ebcba
34r7h <i.34r7h+github@gmail.com>
2024-11-11 17:59:23 +0700

    satswagmi installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SatsWagmiConfig, useConnect } from "@gobob/sats-wagmi";
+
+
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const queryClient = new QueryClient();
+
+function WalletOptions() {
+  const { connectors, connect } = useConnect()
+
+  return connectors.map((connector) => (
+    <button key={connector.name} onClick={() => connect({ connector })}>
+      {connector.name}
+    </button>
+  ))
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <QueryClientProvider client={queryClient}>
+          <SatsWagmiConfig network="testnet" queryClient={queryClient}>
+            <WalletOptions />
+          </SatsWagmiConfig>
+        </QueryClientProvider>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +52,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Enable buffer
+      protocolImports: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      buffer: "buffer",
+    },
+  },
+  optimizeDeps: {
+    include: ["buffer"],
+  },
+});
